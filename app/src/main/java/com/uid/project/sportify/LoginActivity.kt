2bd261510bf8127a2e69b53857e1176a076d3341
Vicondrus <victor.padurean@yahoo.com>
2020-11-20 18:12:11 +0200

    created login activity and layout (not ready)

---
+++ app/src/main/java/com/uid/project/sportify/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.uid.project.sportify
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import android.widget.ListView
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.schedule
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
